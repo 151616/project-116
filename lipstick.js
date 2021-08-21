d1b9b44ee8a5d655722bd1de60776ac9e9c16419
shivansh payak <75551006+151616@users.noreply.github.com>
2021-08-21 11:52:10 -0400

    Add files via upload

---
+++ lipstick.js
@@ -0,0 +1,32 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(400,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400, 300);
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotposes);
+}
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("mouth x= " + results[0].pose.mouth.x);
+        console.log("mouth y=" + results[0].pose.mouth.y);
+    }
+}
+function modelLoaded(){
+    console.log("Posenet is Initialized");
+}
+function draw(){
+image(video, 0, 0, 400, 300);
+
+}
+function take_snapshot(){
+    save("Lipstick_on_your_face.png");
+}
+function mustache(){
+    window.location = "mustache.html";
+}
